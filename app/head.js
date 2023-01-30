0d40b2e074e5907c998298d31870cd2b34b6084d
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-30 20:58:50 +0700

    meta tag

--- app/head.js
+++ app/head.js
@@ -13,9 +13,24 @@ export default function Head() {
                 content="NjlBN0-SfX5SswIfFCO_yhw3bPeuoqdZkygWUPnj1xI"
             ></meta>
             <meta
+                name="keywords"
+                content="rhandyta, briantama, rhandyta briantama, portfolio, web developer, React, HTML, CSS, JavaScript, freelancer, project samples, skills, php, laravel, tailwindcss"
+            ></meta>
+            <meta
                 name="description"
-                content="rhandyta briantama portfolio skill"
+                content="Welcome to my online portfolio. I am a highly skilled web developer specializing in React, HTML, CSS, and JavaScript, PHP, Laravel, Tailwindcss, CodeIgniter. Explore my project samples and see my expertise for yourself."
             />
+            <meta property="og:title" content="Rhandyta Briantama Portfolio" />
+            <meta
+                property="og:description"
+                content="Explore the skills and project samples of a talented web developer specializing in React, HTML, CSS, and JavaScript, PHP, Laravel, Tailwindcss, Codeigniter."
+            />
+            <meta
+                property="og:image"
+                content="https://rhandyta.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fhero.jpg&w=640&q=75"
+            />
+            <meta property="og:url" content="https://rhandyta.vercel.app" />
+            <meta property="og:type" content="website" />
             <meta name="robots" content="all" />
             <link rel="icon" href="/favicon.ico" />
         </>
